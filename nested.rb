168e290028ee1e1a43815b02e8be096e483db43a
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-16 22:59:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 
-programmer_hash[:yukihiro_matsumoto] = { known_for: "Ruby", languages: ["Lisp", "C"]}
 
+programmer_hash[:yukihiro_matsumoto] = { known_for: "Ruby", languages: ["Lisp", "C"]}
+programmer_hash
 
 end
 
